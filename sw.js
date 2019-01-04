5647bf600a0d8512f5eb7a4ec3f91077c288c4dc
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-04 17:42:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-"use strict";importScripts("sw-toolbox.js"),toolbox.precache(["index.html"]),toolbox.router.get("/images/*",toolbox.cacheFirst),toolbox.router.get("/*",toolbox.networkFirst,{networkTimeoutSeconds:5});
+"use strict";importScripts("sw-toolbox.js"),toolbox.precache(["index.html"]),toolbox.router.get("/icons/*",toolbox.cacheFirst),toolbox.router.get("/*",toolbox.networkFirst,{networkTimeoutSeconds:5});
