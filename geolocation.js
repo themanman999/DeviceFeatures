41e006c051f8cf523cc71ac90617245e5b23d7ea
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-04 15:37:09 +0800

    Add files via upload

--- geolocation.js
+++ geolocation.js
@@ -1,20 +1,18 @@
- var Geolocation = document.getElementById('Geolocation'); // Geolocation
-         var watchId;
-         
-         function appendLocation(location, verb) {
-           verb = verb || 'updated';
-           var newLocation = document.createElement('p');
-           newLocation.innerHTML = 'Location ' + verb + ': <a href="https://maps.google.com/maps?&z=15&q=' + location.coords.latitude + '+' + location.coords.longitude + '&ll=' + location.coords.latitude + '+' + location.coords.longitude + '" target="_blank">' + location.coords.latitude + ', ' + location.coords.longitude + '</a>';
-           Geolocation.appendChild(newLocation);
-         }
-         
-         if ('geolocation' in navigator) {
-           document.getElementById('askButton').addEventListener('click', function () {
-             navigator.geolocation.getCurrentPosition(function (location) {
-               appendLocation(location, 'fetched');
-             });
-             watchId = navigator.geolocation.watchPosition(appendLocation);
-           });
-         } else {
-           Geolocation.innerText = 'Geolocation API not supported.';
-         }
+ var Geolocation = document.getElementById('getGeolocation'); // Geolocation
+ var watchId;
+ 
+ function appendLocation(location, verb) {
+   verb = verb || 'updated';
+   Geolocation.innerHTML = 'Location ' + verb + ': <a href="https://maps.google.com/maps?&z=15&q=' + location.coords.latitude + '+' + location.coords.longitude + '&ll=' + location.coords.latitude + '+' + location.coords.longitude + '" target="_blank">' + location.coords.latitude + ', ' + location.coords.longitude + '</a>';
+ }
+ 
+ if ('geolocation' in navigator) {
+   document.getElementById('geolocation').addEventListener('click', function () {
+     navigator.geolocation.getCurrentPosition(function (location) {
+       appendLocation(location, 'fetched');
+     });
+     watchId = navigator.geolocation.watchPosition(appendLocation);
+   });
+ } else {
+   Geolocation.innerText = 'Geolocation API not supported.';
+ }
