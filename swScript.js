62f23f9afce15ee674a0e68c9225f9a15ed9a056
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-07 15:55:24 +0800

    Add files via upload

--- swScript.js
+++ swScript.js
@@ -14,17 +14,3 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.warn('Push messaging is not supported');
     pushButton.textContent = 'Push Not Supported';
 }
-
-self.addEventListener('push', function(event) {
-  console.log('[Service Worker] Push Received.');
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
-  const title = 'Push Codelab';
-  const options = {
-    body: 'Yay it works.',
-    icon: 'images/icon.png',
-    badge: 'images/badge.png'
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
-});
