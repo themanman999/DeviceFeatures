55daa7959c70da4f0abcaa90e02d978bef79105f
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-07 16:42:31 +0800

    Add files via upload

--- localNotification.js
+++ localNotification.js
@@ -15,20 +15,40 @@ function requestPermission() {
   });
 }
 
+
 function nonPersistentNotification() {
-  if (!window.Notification || !Notification.requestPermission)
-      return false;
-  if (Notification.permission == 'granted')
-      throw new Error('You must only call this *before* calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
-  
-    try {
-        new Notification('');
-    } catch (e) {
-        if (e.name == 'TypeError')
-            return false;
-    }
-    return true;
+  if (window.Notification && Notification.permission == 'granted') {
+      // We would only have prompted the user for permission if new
+      // Notification was supported (see below), so assume it is supported.
+      var notification = new Notification("Hi there - non-persistent!");
+  } else if (isNewNotificationSupported()) {
+      // new Notification is supported, so prompt the user for permission.
+          if (!window.Notification || !Notification.requestPermission)
+          return false;
+      if (Notification.permission == 'granted')
+          throw new Error('You must only call this *before* calling Notification.requestPermission(), otherwise this feature detect would bug the user with an actual notification!');
+      try {
+          new Notification('');
+      } catch (e) {
+          if (e.name == 'TypeError')
+              return false;
+      }
+      return true;
+  }
 }
+/*
+function nonPersistentNotification() {
+  if (!('Notification' in window)) {
+    alert('Notification API not supported!');
+    return;
+  }
+  
+  try {
+    var notification = new Notification("Hi there - non-persistent!");
+  } catch (err) {
+    alert('Notification API error: ' + err);
+  }
+}*/
 
 function persistentNotification() {
   if (!('Notification' in window) || !('ServiceWorkerRegistration' in window)) {
