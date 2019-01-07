b74313425d0555450533bad57e9cba4ddbdfe08d
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-07 16:56:12 +0800

    Add files via upload

--- localNotification.js
+++ localNotification.js
@@ -61,6 +61,7 @@ function persistentNotification() {
   try {
     navigator.serviceWorker.getRegistration()
       .then(reg => reg.showNotification("Hi there - persistent!"))
+      console.log(reg);
       .catch(err => alert('Service Worker registration error: ' + err));
   } catch (err) {
     alert('Notification API error: ' + err);
