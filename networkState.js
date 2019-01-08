3303fa8b519fa48903cc3c1e1d303c6c39064746
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-08 15:22:32 +0800

    Add files via upload

--- networkState.js
+++ networkState.js
@@ -1,13 +1,11 @@
-document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline'; // network state
+document.getElementById('isOnline').innerHTML = navigator.onLine ? 'online' : 'offline'; // network state
          
          var networkState = document.getElementById('networkState');
          
          function handleStateChange() {
          	var timeBadge = new Date().toTimeString().split(' ')[0];
-         	var newState = document.createElement('p');
          	var state = navigator.onLine ? 'online' : 'offline';
-         	newState.innerHTML = '<span class="badge">' + timeBadge + '</span> Connection state changed to <b>' + state + '</b>.';
-         	networkState.appendChild(newState);
+         	networkState.innerHTML = '<span class="badge">' + timeBadge + '</span> Connection state changed to <b>' + state + '</b>.';
          }
          
          window.addEventListener('online', handleStateChange);
