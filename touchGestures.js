3f51768caa0e54ea3c558c62cd7b365a2039837b
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-09 15:54:17 +0800

    Add files via upload

--- touchGestures.js
+++ touchGestures.js
@@ -9,13 +9,15 @@ function startDrag(e) {
   var pos = [this.offsetLeft, this.offsetTop];
   var that = this;
   var origin = getCoors(e);
+  var box = document.getElementById('dragBox');
 
   function moveDrag(e) {
     var currentPos = getCoors(e);
     var deltaX = currentPos[0] - origin[0];
     var deltaY = currentPos[1] - origin[1];
-    this.style.left = (pos[0] + deltaX) + 'px';
-    this.style.top = (pos[1] + deltaY) + 'px';
+    
+    this.style.left = (pos[0] + deltaX > -1 && currentPos[0] <= box.offsetWidth) ? (pos[0] + deltaX) + 'px' : (pos[0] + deltaX < 0) ? '0px' : (box.offsetWidth - e.path[0].offsetWidth) + 'px';
+    this.style.top = (pos[1] + deltaY > -1 && (pos[1] + deltaY + e.path[0].offsetHeight) <= box.offsetHeight) ? (pos[1] + deltaY) + 'px' : (pos[1] + deltaY < 0) ? '0px' : (box.offsetHeight - e.path[0].offsetHeight) + 'px';
     return false; // cancels scrolling
   }
 
