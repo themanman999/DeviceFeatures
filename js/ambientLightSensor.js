e409e8c50c1a7806aa87e68516cb31982b7553d7
themanman999 <35715069+themanman999@users.noreply.github.com>
2019-01-10 11:45:38 +0800

    Add files via upload

---
+++ js/ambientLightSensor.js
@@ -0,0 +1,29 @@
+const details = document.getElementsByClassName("ligntSensorLog");
+// Feature detection
+if (window.AmbientLightSensor){
+    try{
+      const sensor = new AmbientLightSensor();
+      // Detect changes in the light
+      sensor.onreading = () => {
+        details[0].innerHTML = sensor.illuminance;
+
+          // Read the light levels in lux 
+          // < 50 is dark room
+          if (sensor.illuminance < 50) {
+            document.body.className = 'darkLight';
+            details[1].innerHTML += 'Dark Light';
+          } else {
+            document.body.className = 'brightLight';
+            details[1].innerHTML += 'Bright Light';
+          }
+      }
+
+      // Has an error occured?
+      sensor.onerror = event => document.getElementsByClassName("ligntSensorLog")[0].innerHTML = event.error.message;
+      sensor.start();
+    } catch(err) {
+      details[0].innerHTML = err.message;
+    }
+} else {
+  details[0].innerHTML = 'It looks like your browser doesnt support this feature'; 
+}
